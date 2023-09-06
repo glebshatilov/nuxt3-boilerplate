4fc4161db71bbe9ab2d1066c0fe04bdcef347f51
glebshatilov <Gleb.Shatilov@technopark.ru>
2023-09-06 14:45:07 +0600

    Updated eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,18 @@
 module.exports = {
-    root: true,
-    env: {
-        browser: true,
-        node: true,
-        es2022: true
-    },
-    extends: [
-        'standard-with-typescript',
-        '@nuxt/eslint-config'
-    ],
-    rules: {
-        '@typescript-eslint/space-before-function-paren': ['error', 'never']
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+    es2022: true
+  },
+  parserOptions: { // fix for js code parser
+    parser: {
+      js: '@typescript-eslint/parser',
+      jsx: '@typescript-eslint/parser',
     }
+  },
+  extends: [
+    'standard-with-typescript',
+    '@nuxt/eslint-config'
+  ]
 }
